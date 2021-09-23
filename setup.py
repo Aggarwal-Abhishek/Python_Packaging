e41d558fc53d9470cd39aca13d95fdbe94999888
aggarwal-abhishek <abhishekaggarwal314@gmail.com>
2021-09-23 12:34:18 +0530

    Creating .whl of .so without .py files

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import os
+import fnmatch
+import sysconfig
 
 from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py as _build_py
+
 from Cython.Build import cythonize
 
 
@@ -18,6 +22,23 @@ def ExtraPaths(root_dir):
                     paths.append(filepath)
     return paths
 
+
+class build_py(_build_py):
+    def find_package_modules(self, package, package_dir):
+        ext_suffix = sysconfig.get_config_vars('EXT_SUFFIX')
+        if isinstance(ext_suffix, list):
+            ext_suffix = ext_suffix[0]
+
+        modules = super().find_package_modules(package, package_dir)
+
+        filtered_modules = []
+        for pkg,mod,filepath in modules:
+            if not os.path.exists(filepath.replace('.py', ext_suffix)):
+                filtered_modules.append((pkg,mod,filepath))
+
+        print('Filtered Modules:', filtered_modules)
+        return filtered_modules
+
 print('Find Packages:', find_packages())
 print('Files to Cythonize:', ExtraPaths('abhi'))
 
@@ -28,7 +49,8 @@ setup(
     ext_modules = cythonize(
         ExtraPaths('abhi'),
         compiler_directives={'language_level': 3}
-    )
+    ),
+    cmdclass={'build_py': build_py}
 )
 
 '''
@@ -38,4 +60,9 @@ tar -xvf
 Now becomes
 
 python setup.py build_ext --inplace
+
+Then
+
+rm -r build
+python setup.py bdist_wheel
 '''
